9873dc3138f5cd107d6d5260586e1d3bbca87b45
shaikot07 <saifulislamshaikat007@gmail.com>
2024-04-03 21:01:37 +0600

    ready to deploy in vercel

--- index.js
+++ index.js
@@ -99,8 +99,8 @@ async function run() {
             res.send(result)
         })
         // Ping MongoDB
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // await client.close();
     }
