851023cec2e3ab169b784299a002fb1cbc69368b
shaikot07 <saifulislamshaikat007@gmail.com>
2024-04-04 01:12:58 +0600

    fix deploy issue

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:5174',
+      "https://heliverse-job-task-project-1.netlify.app",
+      "heliverse-job-task-project-1.netlify.app"
+  
+    ],
+    optionSuccessStatus:200,
+    credentials: true
+  }));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_PASS}@cluster0.loifkbc.mongodb.net/?retryWrites=true&w=majority`;
